2bd1139192961db03d55903a39702939720d9764
passont <mail@thomaspasson.de>
2023-07-12 22:31:43 +0200

    Added first web page layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Active Webhooks</h1>
+    <div v-for="webhook in webhooks" :key="webhook.route" class="webhook">
+      <h2>{{ webhook.repoName }}</h2>
+      <p><strong>Repo URL:</strong> {{ webhook.repoURL }}</p>
+      <p><strong>Route:</strong> {{ webhook.route }}</p>
+      <ul>
+        <li v-for="command in webhook.commands" :key="command">
+          {{ command }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      webhooks: [],
+    };
+  },
+  created() {
+    this.fetchWebhooks();
+  },
+  methods: {
+    async fetchWebhooks() {
+      try {
+        const res = await fetch('/webhooks');
+        this.webhooks = await res.json();
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.webhook {
+  background-color: #f9f9f9;
+  margin-bottom: 1em;
+  padding: 1em;
+  border-radius: 4px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+}
+.webhook h2 {
+  margin: 0;
+  color: #333;
+}
+.webhook p {
+  margin: 0.5em 0;
+  color: #666;
+}
+.webhook ul {
+  margin-top: 0.5em;
+  padding-left: 1em;
 }
 </style>
