f8ba2443d7977f0f221d5716f538375607375f40
passont <mail@thomaspasson.de>
2023-07-20 22:45:35 +0200

    Rafactored easter egg

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -59,15 +59,6 @@ body {
   position: relative;
 }
 
-#matrixCanvas {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  z-index: -1;
-}
-
 .logo {
   display: block;
   width: 200px; 
--- frontend/src/components/MatrixEffect.vue
+++ frontend/src/components/MatrixEffect.vue
@@ -52,3 +52,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#matrixCanvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+}
+</style>
