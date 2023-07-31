e7bbcf57748126e8f04f7a38cbf19e0c81d2cc90
passont <mail@thomaspasson.de>
2023-07-31 19:35:13 +0200

    Added save function for action

--- modules/actions/actions.go
+++ modules/actions/actions.go
@@ -1,6 +1,13 @@
 package actions
 
-import "sw-gittycat-server/modules/webhooks"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sw-gittycat-server/modules/webhooks"
+)
 
 type Action struct {
 	Webhook        *webhooks.Webhook      `yaml:"webhook" json:"webhook"`
@@ -8,4 +15,28 @@ type Action struct {
 	RequestBody    map[string]interface{} `yaml:"request_body" json:"request_body"`
 	LastCall       string                 `yaml:"last_call" json:"last_call"`
 	ProcessingTime string                 `yaml:"processing_time" json:"processing_time"`
+}
+
+func (a *Action) Save() error {
+	after, ok := a.RequestBody["after"].(string)
+	if !ok || len(after) == 0 {
+		return errors.New("invalid or missing 'after' field in request body")
+	}
+
+	// Prepare JSON data
+	data, err := json.Marshal(a)
+	if err != nil {
+		return err
+	}
+
+	// Create the file name
+	fileName := filepath.Join("/actions", fmt.Sprintf("%s.json", after))
+
+	// Create the file
+	err = os.WriteFile(fileName, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
