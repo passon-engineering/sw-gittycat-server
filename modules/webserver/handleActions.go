dc405b77a405cb8c584463cea8cd30bc76fd4295
passont <mail@thomaspasson.de>
2023-08-01 22:41:00 +0200

    Repaired actions web handler

--- modules/webserver/handleActions.go
+++ modules/webserver/handleActions.go
@@ -2,13 +2,11 @@ package webserver
 
 import (
 	"encoding/json"
-	"math"
 	"net/http"
 	"sw-gittycat-server/modules/application"
 	"time"
 
 	"github.com/passon-engineering/sw-go-logger-lib/logger"
-	"github.com/passon-engineering/sw-go-utility-lib/file"
 )
 
 func handleActions(app *application.Application) func(http.ResponseWriter, *http.Request) {
@@ -16,21 +14,10 @@ func handleActions(app *application.Application) func(http.ResponseWriter, *http
 		startTime := time.Now()
 
 		w.Header().Set("Content-Type", "application/json")
-
-		stats, err := file.CountFilesAndFolders(app.ServerPath+"/actions", math.MaxInt64)
-		if err != nil {
-			app.Logger.Entry(logger.Container{
-				Status:         logger.STATUS_ERROR,
-				Error:          "Could not read actions: " + err.Error(),
-				HttpRequest:    r,
-				ProcessingTime: time.Since(startTime),
-			})
-		}
-
-		json.NewEncoder(w).Encode(stats)
+		json.NewEncoder(w).Encode(app.ActionHandler.Actions)
 		app.Logger.Entry(logger.Container{
 			Status:         logger.STATUS_INFO,
-			Info:           "Actions stats transmitted",
+			Info:           "Listed available actions",
 			HttpRequest:    r,
 			ProcessingTime: time.Since(startTime),
 		})
