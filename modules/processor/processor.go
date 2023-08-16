3c9bad38e208f43fb5e4a9db7963d26082985e12
passont <mail@thomaspasson.de>
2023-08-16 22:17:26 +0200

    Implemented command output collector to processor and fixed some wrong code

--- modules/processor/processor.go
+++ modules/processor/processor.go
@@ -25,13 +25,17 @@ func processActionQueue(action actions.Action, app *application.Application) {
 
 	for _, repo := range action.Webhook.Repos {
 		git.CloneRepo(repo.RepoURL, repo.RepoName, app)
+		action.AppendOutput("cloned repository:", repo.RepoName)
 		git.PullRepo(repo.RepoName, app)
+		action.AppendOutput("pulled repository:", repo.RepoName)
 	}
 
 	commandHandlerInner := func(repoName string, command string, output string, err error) {
 		if err != nil {
 			success = false
 			output := fmt.Sprintf("'%s': Command '%s' failed: %v", repoName, command, err)
+
+			action.AppendOutput(output)
 			app.Logger.Entry(logger.Container{
 				Status:         logger.STATUS_ERROR,
 				Error:          output,
@@ -39,6 +43,8 @@ func processActionQueue(action actions.Action, app *application.Application) {
 			})
 		} else {
 			output := fmt.Sprintf("'%s': Command '%s' executed successfully, output: %s", repoName, command, output)
+
+			action.AppendOutput(output)
 			app.Logger.Entry(logger.Container{
 				Status:         logger.STATUS_INFO,
 				Info:           output,
@@ -52,23 +58,17 @@ func processActionQueue(action actions.Action, app *application.Application) {
 		if err != nil {
 			success = false
 			output := fmt.Sprintf("'%s': Command '%s' failed: %v", action.Webhook.BuildName, command, err)
+
+			action.AppendOutput(output)
 			app.Logger.Entry(logger.Container{
 				Status:         logger.STATUS_ERROR,
 				Error:          output,
 				ProcessingTime: time.Since(startTime),
 			})
 		} else {
-			after, ok := action.RequestBody["after"].(string)
-			if !ok || len(after) == 0 {
-				after = "no_hash_available"
-			}
-
-			elapsedTime := time.Since(startTime)
-			action.ProcessingTime = elapsedTime.String()
-
-			action.FileName = startTime.Format("2006-01-02_15-04-05") + "_" + action.Webhook.BuildName + "_" + after + ".json"
-
 			output := fmt.Sprintf("'%s': Command '%s' executed successfully, output: %s, processing time: %s", action.Webhook.BuildName, command, output, action.ProcessingTime)
+
+			action.AppendOutput(output)
 			app.Logger.Entry(logger.Container{
 				Status:         logger.STATUS_INFO,
 				Info:           output,
@@ -83,5 +83,13 @@ func processActionQueue(action actions.Action, app *application.Application) {
 	action.ProcessingTime = elapsedTime.String()
 
 	action.Success = success
+
+	after, ok := action.RequestBody["after"].(string)
+	if !ok || len(after) == 0 {
+		after = "no_hash_available"
+	}
+
+	action.FileName = startTime.Format("2006-01-02_15-04-05") + "_" + action.Webhook.BuildName + "_" + after + ".json"
+
 	app.ActionHandler.Add(&action)
 }
