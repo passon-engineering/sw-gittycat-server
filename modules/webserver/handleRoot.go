a45ad96ef3fe171f141f0430155736019f3f8d6d
passont <mail@thomaspasson.de>
2023-08-24 22:57:37 +0200

    Changed root web handler

--- modules/webserver/handleRoot.go
+++ modules/webserver/handleRoot.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/passon-engineering/sw-go-logger-lib/logger"
-	"github.com/passon-engineering/sw-go-utility-lib/web"
 )
 
 func handleRoot(app *application.Application) http.HandlerFunc {
@@ -15,18 +14,16 @@ func handleRoot(app *application.Application) http.HandlerFunc {
 
 		path := r.URL.Path[1:]
 		if path == "" {
-			http.Redirect(w, r, "/index.html", http.StatusSeeOther)
-
-			app.Logger.Entry(logger.Container{
-				Status:         logger.STATUS_INFO,
-				Source:         "root_redirect",
-				HttpRequest:    r,
-				ProcessingTime: time.Since(startTime),
-			})
-			return
+			path = "index.html"
 		}
 
-		web.ServeStaticFile(w, r, app.ServerPath+"/frontend/dist/"+path)
-
+		http.ServeFile(w, r, app.ServerPath+app.Config.WebDirectory+path)
+		app.Logger.Entry(logger.Container{
+			Status:         logger.STATUS_INFO,
+			Source:         "handleRoot",
+			Info:           "served static file",
+			HttpRequest:    r,
+			ProcessingTime: time.Since(startTime),
+		})
 	}
 }
