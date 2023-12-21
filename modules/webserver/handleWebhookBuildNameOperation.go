c1de945d443cf5462eeeb2e9211b57a5979b4ab2
passont <mail@thomaspasson.de>
2023-12-21 22:55:09 +0100

    Removed unused webhook processing

--- modules/webserver/handleWebhookBuildNameOperation.go
+++ modules/webserver/handleWebhookBuildNameOperation.go
@@ -1,12 +1,8 @@
 package webserver
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 	"strconv"
-	"sw-gittycat-server/modules/actions"
 	"sw-gittycat-server/modules/application"
 	"time"
 
@@ -60,35 +56,5 @@ func handleWebhookRepoNameOperation(app *application.Application) func(http.Resp
 
 			return
 		}
-
-		// Read the request body
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			// Handle the error
-			// You can return an error response or take appropriate action
-			fmt.Println("Error reading request body:", err.Error())
-			return
-		}
-
-		// Parse the request body into a map[string]interface{}
-		var requestBody map[string]interface{}
-		err = json.Unmarshal(body, &requestBody)
-		if err != nil {
-			// Handle the error
-			// You can return an error response or take appropriate action
-			fmt.Println("Error parsing request body:", err.Error())
-			requestBody = nil
-		}
-
-		timestamp := time.Now().Format(time.RFC3339)
-		// Add the webhook action to the queue
-		app.Queue <- actions.Action{Webhook: webhook, RequestBody: requestBody, LastCall: timestamp, Output: "Action triggered: " + timestamp}
-
-		app.Logger.Entry(logger.Container{
-			Status:         logger.STATUS_INFO,
-			Info:           "Webhook: ",
-			HttpRequest:    r,
-			ProcessingTime: time.Since(startTime),
-		})
 	}
 }
